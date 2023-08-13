a504834e9f64f1bfe4d6b53fb356d25419be7cb7
Kirill-cmd2 <73397254+Kirill-cmd2@users.noreply.github.com>
2023-08-13 10:01:57 +0500

    Started using storage

--- loader.py
+++ loader.py
@@ -5,11 +5,13 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ParseMode
 
 from config import TOKEN
+from database import MyDatabase
 
 
 BOT = Bot(TOKEN, parse_mode = ParseMode.HTML)
 DP = Dispatcher(BOT, storage = MemoryStorage())
+DB = MyDatabase()
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s', level=logging.INFO)
 
-__all__ = ['BOT', 'DP']
+__all__ = ['BOT', 'DP', 'DB']
