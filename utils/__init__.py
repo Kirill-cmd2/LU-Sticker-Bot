eb3e404565433fcbb9a210eb12d22f89405fa97f
Abdulla <bubishincosmos29@gmail.com>
2024-11-30 14:29:55 +0500

    renamed work_with_photo module's method import

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,4 @@ from .default_commands import set_default_commands_with_language_code
 from .notify_about_err import err_notify_admins
 from .states import cnssStates, anstsStates, delstickerStates
 from .throttling_decorator import rate_limit
-from .work_with_photo import processes_on_photo
+from .work_with_photo import get_resized_photo
