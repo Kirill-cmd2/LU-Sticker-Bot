0c64ab34ab195183dbe0b2a2ed797c3cab759c2e
Abdulla <bubishincosmos29@gmail.com>
2024-12-01 12:20:44 +0500

    importing get_resized_photo from process_photo

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,4 @@ from .default_commands import set_default_commands_with_language_code
 from .notify_about_err import err_notify_admins
 from .states import cnssStates, anstsStates, delstickerStates
 from .throttling_decorator import rate_limit
-from .work_with_photo import get_resized_photo
+from .process_photo import get_resized_photo
